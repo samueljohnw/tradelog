c0ee278ff413864441a53a35406b4d03730c3fe4
Sam <samueljwerner@gmail.com>
2018-02-04 14:54:54 -0600

    add average score

--- resources/views/trade/index.blade.php
+++ resources/views/trade/index.blade.php
@@ -1,28 +1,35 @@
 @extends('template.full')
 @section('content')
+
 <nav class="level">
   <div class="level-item has-text-centered">
     <div>
-      <p class="heading">Last 7 Days Wins / Losses</p>
-      <p class="title"><span style="color:green">{{$weekProgressData->where('status','win')->count()}}</span> / <span style="color:red">{{$weekProgressData->where('status','loss')->count()}}</span></p>
+      <p class="heading">Last 7 Days<br/> Total / Wins / Losses</p>
+      <p class="title"> {{$weekProgressData->count()}} / <span style="color:green">{{$weekProgressData->where('status','win')->count()}}</span> / <span style="color:red">{{$weekProgressData->where('status','loss')->count()}}</span></p>
     </div>
   </div>
   <div class="level-item has-text-centered">
     <div>
-      <p class="heading">Last 7 Days Profit / Loss</p>
+      <p class="heading">Last 7 Days<br/> Profit / Loss</p>
       <p class="title"><span style="color:green">${{number_format($weekProgressData->where('status','win')->sum('pl'))}}</span> / <span style="color:red">${{number_format($weekProgressData->where('status','loss')->sum('pl'))}}</span></p>
     </div>
   </div>
   <div class="level-item has-text-centered">
     <div>
-      <p class="heading">Last 30 Days Wins / Losses</p>
-      <p class="title"><span style="color:green">{{$monthProgressData->where('status','win')->count()}}</span> / <span style="color:red">{{$monthProgressData->where('status','loss')->count()}}</span></p>
+      <p class="heading">Last 30 Days<br/> Total/ Wins / Losses</p>
+      <p class="title">{{$monthProgressData->count()}} / <span style="color:green">{{$monthProgressData->where('status','win')->count()}}</span> / <span style="color:red">{{$monthProgressData->where('status','loss')->count()}}</span></p>
     </div>
   </div>
   <div class="level-item has-text-centered">
     <div>
       <p class="heading">Last 30 Days Profit / Loss</p>
       <p class="title"><span style="color:green">${{number_format($monthProgressData->where('status','win')->sum('pl'))}} </span>/ <span style="color:red">${{number_format($monthProgressData->where('status','loss')->sum('pl'))}}</span></p>
+    </div>
+  </div>
+  <div class="level-item has-text-centered">
+    <div>
+      <p class="heading">Last 30 Days Average Win / Loss</p>
+      <p class="title"><span style="color:green">{{$monthProgressData->where('status','win')->avg('pl')}}</span> / <span style="color:red">{{$monthProgressData->where('status','loss')->avg('pl')}}</span></p>
     </div>
   </div>
 </nav>
@@ -36,7 +43,7 @@
             <th>Symbol</th>
             <th>Images</th>
             <th>P/L</th>
-            <th>Date</th>
+            <th class="date">Date</th>
         </tr>
     </thead>
     <tbody>
@@ -74,7 +81,7 @@
         ${{$trade->pl}}
         @endif
       </td>
-      <td>
+      <td class="date">
         {{$trade->datetime()}}
       </td>
     </tr>
